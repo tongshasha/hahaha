24e31b56d972192d12fa9ddd129a7b18884c53c1
tongshasha <qfnutss@163.com>
2016-07-22 09:21:10 +0800

    Initial Commit

---
+++ hahaha/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hahaha
+//
+//  Created by 佟莎莎 on 16/7/22.
+//  Copyright © 2016年 asha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hahaha/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hahaha
+//
+//  Created by 佟莎莎 on 16/7/22.
+//  Copyright © 2016年 asha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
